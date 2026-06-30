ff5a3d80e81f236d07b87763789675a9eec06214
Gustavo Rodrigues Gutierrez <gustavo,g@digitalnet.local>
2026-06-30 13:58:22 -0400

    fix(middleware): use authorized callback to allow /admin/setup without session

--- middleware.ts
+++ middleware.ts
@@ -1,7 +1,15 @@
 import { withAuth } from 'next-auth/middleware';
 
-export default withAuth({ pages: { signIn: '/admin/login' } });
+export default withAuth({
+  pages: { signIn: '/admin/login' },
+  callbacks: {
+    authorized({ req, token }) {
+      if (req.nextUrl.pathname.startsWith('/admin/setup')) return true;
+      return !!token;
+    },
+  },
+});
 
 export const config = {
-  matcher: ['/admin/((?!login|setup).*)'],
+  matcher: ['/admin/((?!login).*)'],
 };
